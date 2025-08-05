bad20234ad074df1e08cb965ec09895b946453f9
David Bindel <bindel@cornell.edu>
2025-08-05 13:26:45 -0400

    Refactor shapes.

--- src/shapes.jl
+++ src/shapes.jl
@@ -1,36 +1,10 @@
-function shapes1dP1(x)
-    0.5*(1-x),
-    0.5*(1+x)
-end
+# -- 1D shapes and derivatives
 
-function dshapes1dP1(x)
-    -0.5,
-    0.5
-end
+shapes1dP1(x) = (0.5*(1-x), 0.5*(1+x))
+dshapes1dP1(x) =(-0.5,      0.5)
 
-function shapes1dP1!(N, dN, xx)
-    x = xx[1]
-    N[:]  .= shapes1dP1(x)
-    dN[:] .= dshapes1dP1(x)
-end
-
-function shapes1dP2(x)
-    -0.5*(1-x)*x,
-         (1-x)*  (1+x),
-     0.5*      x*(1+x)
-end
-
-function dshapes1dP2(x)
-    -0.5*(1-2*x),
-    -2*x,
-     0.5*(1+2*x)
-end
-
-function shapes1dP2!(N, dN, xx)
-    x = xx[1]
-    N[:]  .= shapes1dP2(x)
-    dN[:] .= dshapes1dP2(x)
-end
+shapes1dP2(x) = (-0.5*(1-x)*x, (1-x)*(1+x), 0.5*x*(1+x))
+dshapes1dP2(x) =(-0.5*(1-2*x), -2*x,        0.5*(1+2*x))
 
 function shapes1dP3(x)
     (-1.0/16) * (1-x)*(1-3*x)*(1+3*x),
@@ -46,56 +20,109 @@ function dshapes1dP3(x)
     1.0/16 * (-1+x*( 18+x* 27))
 end
 
-function shapes1dP3!(N, dN, xx)
-    x = xx[1]
-    N[:]  .= shapes1dP3(x)
-    dN[:] .= dshapes1dP3(x)
+# -- Shape function class setup
+
+macro make_shape(classname, nfun, dim, body)
+    quote
+        struct $(esc(classname))
+            N  :: Vector{Float64}
+            dN :: Matrix{Float64}
+        end
+
+        $(esc(classname))() =
+            $(esc(classname))(zeros($nfun), zeros($nfun, $dim))
+
+        $(esc(:nshapes))( :: $(esc(classname))) = $nfun
+        $(esc(:dshapes))( :: $(esc(classname))) = $dim
+
+        function (s :: $(esc(classname)))(xx)
+            $body
+            s
+        end
+    end
 end
 
-function shapes2dP1!(N, dN, xx)
+# -- Shape function class definitions
+
+@make_shape Shapes1dP1 2 1 begin
+    x = xx[1]
+    s.N[:]  .= shapes1dP1(x)
+    s.dN[:] .= dshapes1dP1(x)
+end
+refnodes(:: Shapes1dP1) = [-1.0 1.0]
+
+@make_shape Shapes1dP2 3 1 begin
+    x = xx[1]
+    s.N[:]  .= shapes1dP2(x)
+    s.dN[:] .= dshapes1dP2(x)
+end
+refnodes(:: Shapes1dP2) = [-1.0 0.0 1.0]
+
+@make_shape Shapes1dP3 4 1 begin
+    x = xx[1]
+    s.N[:]  .= shapes1dP3(x)
+    s.dN[:] .= dshapes1dP3(x)
+end
+refnodes(:: Shapes1dP3) = [-1.0 -1.0/3 1.0/3 1.0]
+
+@make_shape Shapes2dP1 4 2 begin
     Nx1,  Nx2  = shapes1dP1(xx[1])
     Ny1,  Ny2  = shapes1dP1(xx[2])
     dNx1, dNx2 = dshapes1dP1(xx[1])
     dNy1, dNy2 = dshapes1dP1(xx[2])
-    N[:]  .= ( Nx1* Ny1,  Nx2* Ny1,  Nx2* Ny2,  Nx1* Ny2)
-    dN[:] .= (dNx1* Ny1, dNx2* Ny1, dNx2* Ny2, dNx1* Ny2,
-               Nx1*dNy1,  Nx2*dNy1,  Nx2*dNy2,  Nx1*dNy2)              
+    s.N[:]  .= ( Nx1* Ny1,  Nx2* Ny1,  Nx2* Ny2,  Nx1* Ny2)
+    s.dN[:] .= (dNx1* Ny1, dNx2* Ny1, dNx2* Ny2, dNx1* Ny2,
+                 Nx1*dNy1,  Nx2*dNy1,  Nx2*dNy2,  Nx1*dNy2)
 end
+refnodes(:: Shapes2dP1) =
+    [-1.0   1.0   1.0  -1.0 ;
+     -1.0  -1.0   1.0   1.0 ]
 
-function shapes2dP2!(N, dN, xx)
+@make_shape Shapes2dP2 9 2 begin
     Nx1,  Nx2,  Nx3  = shapes1dP2(xx[1])
     Ny1,  Ny2,  Ny3  = shapes1dP2(xx[2])
     dNx1, dNx2, dNx3 = dshapes1dP2(xx[1])
     dNy1, dNy2, dNy3 = dshapes1dP2(xx[2])
-    N[:] .=  ( Nx1* Ny1,  Nx2* Ny1,  Nx3* Ny1,
-               Nx3* Ny2,  Nx3* Ny3,  Nx2* Ny3,
-               Nx1* Ny3,  Nx1* Ny2,  Nx2* Ny2)
-    dN[:] .= (dNx1* Ny1, dNx2* Ny1, dNx3* Ny1,
-              dNx3* Ny2, dNx3* Ny3, dNx2* Ny3,
-              dNx1* Ny3, dNx1* Ny2, dNx2* Ny2,
-               Nx1*dNy1,  Nx2*dNy1,  Nx3*dNy1,
-               Nx3*dNy2,  Nx3*dNy3,  Nx2*dNy3,
-               Nx1*dNy3,  Nx1*dNy2,  Nx2*dNy2)
+    s.N[:] .=  ( Nx1* Ny1,  Nx2* Ny1,  Nx3* Ny1,
+                 Nx3* Ny2,  Nx3* Ny3,  Nx2* Ny3,
+                 Nx1* Ny3,  Nx1* Ny2,  Nx2* Ny2)
+    s.dN[:] .= (dNx1* Ny1, dNx2* Ny1, dNx3* Ny1,
+                dNx3* Ny2, dNx3* Ny3, dNx2* Ny3,
+                dNx1* Ny3, dNx1* Ny2, dNx2* Ny2,
+                 Nx1*dNy1,  Nx2*dNy1,  Nx3*dNy1,
+                 Nx3*dNy2,  Nx3*dNy3,  Nx2*dNy3,
+                 Nx1*dNy3,  Nx1*dNy2,  Nx2*dNy2)
 end
+refnodes(:: Shapes2dP2) =
+    [-1.0   0.0   1.0   1.0   1.0   0.0  -1.0  -1.0   0.0 ;
+     -1.0  -1.0  -1.0   0.0   1.0   1.0   1.0   0.0   0.0 ]
 
-function shapes2dS2!(N, dN, xx)
-    Nx1,  Nx2,  Nx3  = shapes1dP2(xx[1])
-    Ny1,  Ny2,  Ny3  = shapes1dP2(xx[2])
-    dNx1, dNx2, dNx3 = dshapes1dP2(xx[1])
-    dNy1, dNy2, dNy3 = dshapes1dP2(xx[2])
-    N[:] .=  ( Nx1* Ny1,  Nx2* Ny1,  Nx3* Ny1,
-               Nx3* Ny2,  Nx3* Ny3,  Nx2* Ny3,
-               Nx1* Ny3,  Nx1* Ny2)
-    dN[:] .= (dNx1* Ny1, dNx2* Ny1, dNx3* Ny1,
-              dNx3* Ny2, dNx3* Ny3, dNx2* Ny3,
-              dNx1* Ny3, dNx1* Ny2,
-               Nx1*dNy1,  Nx2*dNy1,  Nx3*dNy1,
-               Nx3*dNy2,  Nx3*dNy3,  Nx2*dNy3,
-               Nx1*dNy3,  Nx1*dNy2)
+@make_shape Shapes2dS2 8 2 begin
+    x, y = xx
+    Nx1,  Nx2,  Nx3  = shapes1dP2(x)
+    Ny1,  Ny2,  Ny3  = shapes1dP2(y)
+    dNx1, dNx2, dNx3 = dshapes1dP2(x)
+    dNy1, dNy2, dNy3 = dshapes1dP2(y)
+    s.N[:] .=  ( Nx1* Ny1,  Nx2* Ny1,  Nx3* Ny1,
+                 Nx3* Ny2,  Nx3* Ny3,  Nx2* Ny3,
+                 Nx1* Ny3,  Nx1* Ny2)
+    s.dN[:] .= (dNx1* Ny1, dNx2* Ny1, dNx3* Ny1,
+                dNx3* Ny2, dNx3* Ny3, dNx2* Ny3,
+                dNx1* Ny3, dNx1* Ny2,
+                 Nx1*dNy1,  Nx2*dNy1,  Nx3*dNy1,
+                 Nx3*dNy2,  Nx3*dNy3,  Nx2*dNy3,
+                 Nx1*dNy3,  Nx1*dNy2)
 end
+refnodes(:: Shapes2dS2) =
+    [-1.0   0.0   1.0   1.0   1.0   0.0  -1.0  -1.0   0.0 ;
+     -1.0  -1.0  -1.0   0.0   1.0   1.0   1.0   0.0   0.0 ]
 
-function shapes2dT1!(N, dN, xx)
-    N[:]  .= (1.0-xx[1]-xx[2], xx[1], xx[2])
-    dN[:] .= (-1.0, 1.0, 0.0,
-              -1.0, 0.0, 1.0)
+@make_shape Shapes2dT1 3 2 begin
+    x, y = xx
+    s.N[:]  .= (1.0-x-y, x, y)
+    s.dN[:] .= (-1.0, 1.0, 0.0,
+                -1.0, 0.0, 1.0)
 end
+refnodes(:: Shapes2dT1) =
+    [ 0.0 1.0 0.0 ;
+      0.0 0.0 1.0 ]
--- test/test_shapes.jl
+++ test/test_shapes.jl
@@ -3,60 +3,49 @@ include("../src/shapes.jl")
 using Test
 using LinearAlgebra
 
-function test_shape_lagrange(shape!, nodes)
-    d, nen = size(nodes)
-    N  = zeros(nen)
-    dN = zeros(nen, d)
-    L  = zeros(nen, nen)
+function test_shape_lagrange(shapes)
+    nen = nshapes(shapes)
+    nodes = refnodes(shapes)
+    L = zeros(nen, nen)
     for j = 1:nen
-        shape!(N, dN, nodes[:,j])
-        L[:,j] .= N[:]
+        L[:,j] .= shapes(nodes[:,j]).N[:]
     end
     @test L ≈ I
 end
 
-function test_dshape(shape!, x0, nen)
-    d = length(x0)
-    N, Np, Nm = zeros(nen), zeros(nen), zeros(nen)
-    dN = zeros(nen, d)
-    h  = 1e-6
-    for j = 1:d
+function test_dshape(shapes, x0)
+    h = 1e-6
+    for j = 1:dshapes(shapes)
         xp, xm = copy(x0), copy(x0)
         xp[j] = x0[j]+h
         xm[j] = x0[j]-h
-        shape!(Np, dN, xp)
-        shape!(Nm, dN, xm)
-        shape!(N, dN, x0)
-        @test dN[:,j] ≈ (Np-Nm)/(2h)  atol=1e-8
+        Np = copy(shapes(xp).N)
+        Nm = copy(shapes(xm).N)
+        @test shapes(x0).dN[:,j] ≈ (Np-Nm)/(2h)  atol=1e-8
     end
 end
 
 @testset "Lagrange property" begin
-test_shape_lagrange(shapes1dP1!, [-1.0 1.0])
-test_shape_lagrange(shapes1dP2!, [-1.0 0.0 1.0])
-test_shape_lagrange(shapes1dP3!, [-1.0 -1.0/3 1.0/3 1.0])
-test_shape_lagrange(shapes2dP1!,
-        [-1.0   1.0   1.0  -1.0 ;
-         -1.0  -1.0   1.0   1.0])
-test_shape_lagrange(shapes2dP2!,
-        [-1.0   0.0   1.0   1.0   1.0   0.0  -1.0  -1.0   0.0 ;
-         -1.0  -1.0  -1.0   0.0   1.0   1.0   1.0   0.0   0.0 ])
-test_shape_lagrange(shapes2dS2!,
-        [-1.0   0.0   1.0   1.0   1.0   0.0  -1.0  -1.0 ;
-         -1.0  -1.0  -1.0   0.0   1.0   1.0   1.0   0.0 ])
+    test_shape_lagrange(Shapes1dP1())
+    test_shape_lagrange(Shapes1dP2())
+    test_shape_lagrange(Shapes1dP3())
+    test_shape_lagrange(Shapes2dP1())
+    test_shape_lagrange(Shapes2dP2())
+    test_shape_lagrange(Shapes2dS2())
+    test_shape_lagrange(Shapes2dT1())
 end
 
 @testset "1D shape derivatives" begin
-x1test = [0.876]
-test_dshape(shapes1dP1!, x1test, 2)
-test_dshape(shapes1dP2!, x1test, 3)
-test_dshape(shapes1dP3!, x1test, 4)
+    x1test = [0.876]
+    test_dshape(Shapes1dP1(), x1test)
+    test_dshape(Shapes1dP2(), x1test)
+    test_dshape(Shapes1dP3(), x1test)
 end
 
 @testset "2D shape derivatives" begin
-x2test = [0.1312; 0.2488]
-test_dshape(shapes2dP1!, x2test, 4)
-test_dshape(shapes2dP2!, x2test, 9)
-test_dshape(shapes2dS2!, x2test, 8)
-test_dshape(shapes2dT1!, x2test, 3)
+    x2test = [0.1312; 0.2488]
+    test_dshape(Shapes2dP1(), x2test)
+    test_dshape(Shapes2dP2(), x2test)
+    test_dshape(Shapes2dS2(), x2test)
+    test_dshape(Shapes2dT1(), x2test)
 end
