2b1158098944e2a9fc04a91d26c9388f84cd2c80
David Bindel <bindel@cornell.edu>
2025-08-05 22:56:31 -0400

    Mesh now passing tests.

--- src/mesh.jl
+++ src/mesh.jl
@@ -27,7 +27,7 @@ function mesh_block2d_P1(nex, ney)
     mesh = Mesh(Shapes2dP1(), nx*ny, nex*ney)
 
     # Set up nodes (row-by-row, SW to NE)
-    for iy = 1:nx
+    for iy = 1:ny
         for ix = 1:nx
             i = ix + (iy-1)*nx
             mesh.X[:,i] .= ( (ix-1)/(nx-1), (iy-1)/(ny-1) )
@@ -39,10 +39,10 @@ function mesh_block2d_P1(nex, ney)
         for ix=1:nex
             i = ix + (iy-1)*nex
             i_sw = ix + (iy-1)*(nex+1)
-            mesh.elt[:,i] = (i_sw,
-                             i_sw + 1,
-                             i_sw + 1 + nex+1,
-                             i_sw + nex+1)
+            mesh.elt[:,i] .= (i_sw,
+                              i_sw + 1,
+                              i_sw + 1 + nex+1,
+                              i_sw + nex+1)
         end
     end
 
@@ -57,7 +57,7 @@ function mesh_block2d_P2(nex, ney)
     for iy = 1:ny
         for ix = 1:nx
             i = ix + (iy-1)*nx
-            mesh[:,i] .= ( (ix-1)/(nx-1), (iy-1)/(ny-1) )
+            mesh.X[:,i] .= ( (ix-1)/(nx-1), (iy-1)/(ny-1) )
         end
     end
 
@@ -66,15 +66,15 @@ function mesh_block2d_P2(nex, ney)
         for ix = 1:nex
             i = ix + (iy-1)*nex
             i_sw = 2*(ix-1) + 2*(iy-1)*nx + 1
-            mesh.elt[:,i] = (i_sw,
-                             i_sw + 1,
-                             i_sw + 2,
-                             i_sw + 2 + nx,
-                             i_sw + 2 + 2*nx,
-                             i_sw + 1 + 2*nx,
-                             i_sw +   + 2*nx,
-                             i_sw +   + nx,
-                             i_sw + 1 + nx)
+            mesh.elt[:,i] .= (i_sw,
+                              i_sw + 1,
+                              i_sw + 2,
+                              i_sw + 2 + nx,
+                              i_sw + 2 + 2*nx,
+                              i_sw + 1 + 2*nx,
+                              i_sw +   + 2*nx,
+                              i_sw +   + nx,
+                              i_sw + 1 + nx)
         end
     end
     
@@ -84,7 +84,7 @@ end
 function mesh_block2d_S2(nex, ney)
     nx0, nx1 = 2*nex+1, nex+1  # Even/odd row sizes
     numnp = (ney+1)*nx0 + ney*nx1
-    mesh = Mesh(Shape2dS2(), numnp, nex*ney)
+    mesh = Mesh(Shapes2dS2(), numnp, nex*ney)
 
     # Set up nodes (row-by-row, SW to NE)
     for iy = 1:ney
@@ -92,14 +92,20 @@ function mesh_block2d_S2(nex, ney)
 
         # Fill bottom row
         for ix = 1:nx0
-            mesh.X[:,start+ix] = ( (ix-1)/(nx0-1), (iy-1)/ney )
+            mesh.X[:,start+ix] .= ( (ix-1)/(nx0-1), (iy-1)/ney )
         end
 
         # Fill middle row
         start += nx0
         for ix = 1:nx1
-            mesh.X[:,start+ix] = ( (ix-1)/(nx1-1), (iy-0.5)/ney )
+            mesh.X[:,start+ix] .= ( (ix-1)/(nx1-1), (iy-0.5)/ney )
         end
+    end
+    
+    # Fill top row
+    start = ney*(nx0+nx1)
+    for ix = 1:nx0
+        mesh.X[:,start+ix] .= ( (ix-1)/(nx0-1), 1.0 )
     end
 
     # Set up element connectivity
@@ -109,14 +115,14 @@ function mesh_block2d_S2(nex, ney)
             i_sw = 2*(ix-1) + (iy-1)*(nx0+nx1) + 1
             i_ww =   (ix-1) + (iy-1)*(nx0+nx1) + nx0 + 1
             i_nw = 2*(ix-1) + (iy-1)*(nx0+nx1) + nx0 + nx1 + 1
-            mesh.elt[:,i] = (i_sw,
-                             i_sw + 1,
-                             i_sw + 2,
-                             i_ww + 1,
-                             i_nw + 2,
-                             i_nw + 1,
-                             i_nw,
-                             i_ww)
+            mesh.elt[:,i] .= (i_sw,
+                              i_sw + 1,
+                              i_sw + 2,
+                              i_ww + 1,
+                              i_nw + 2,
+                              i_nw + 1,
+                              i_nw,
+                              i_ww)
         end
     end
 
@@ -131,7 +137,7 @@ function mesh_block2d_T1(nex, ney)
     for iy = 1:ney+1
         for ix = 1:nex+1
             i = ix + (iy-1)*(nex+1)
-            mesh.X[:,i] = ( (ix-1)/(nx-1), (iy-1)/(ny-1) )
+            mesh.X[:,i] .= ( (ix-1)/(nx-1), (iy-1)/(ny-1) )
         end
     end
 
@@ -142,34 +148,29 @@ function mesh_block2d_T1(nex, ney)
             i_sw = ix + (iy-1)*(nex+1);
 
             # Two triangles makes a square
-            mesh.elt[:,2*i-1] = (i_sw, i_sw + 1, i_sw + nex+1)
-            mesh.elt[:,2*i  ] = (i_sw + nex+1, i_sw + 1, i_sw + 1 + nex+1)
+            mesh.elt[:,2*i-1] .= (i_sw, i_sw + 1, i_sw + nex+1)
+            mesh.elt[:,2*i  ] .= (i_sw + nex+1, i_sw + 1, i_sw + 1 + nex+1)
         end
     end
 
     mesh
 end
 
-function mesh_to_spatial!(mesh, eltid, xref, F)
-    s = mesh.shapes(xref)
-    xref[:] .= 0.0
-    F.factors[:] .= 0.0
-    for k = 1:nshapes(s)
-        j = mesh.elt[k,eltid]
-        Nj = N[j]
-        xj = @view mesh.X[:,j]
-        dNj = @view dN[j,:]
-        @. xref[:] += xj * s.N[j]
-        mul!(F.factors, xj, dNj, 1.0, 1.0)
-    end
-    LAPACK.getrf!(F.factors, F.ipiv)
+function mesh_to_spatial!(mesh, eltid, x, J)
+    s = mesh.shapes(x)
+    e = view(mesh.elt,:,eltid)
+    mul!(x, view(mesh.X,:,e), s.N, 1.0, 0.0)
+    mul!(J, view(mesh.X,:,e), s.dN, 1.0, 0.0)
+    F = lu(J)
     rdiv!(s.dN, F)
     x, F, det(F)
+    
 end
 
 function mesh_to_spatial(mesh, eltid, xref)
-    F = LU(zeros(d,d), zeros(Integer,d), zero(LAPACK.BlasInt))
-    mesh_to_spatial!(mesh, eltid, copy(xref), F)
+    d = dshapes(mesh.shapes)
+    J = zeros(d,d)
+    mesh_to_spatial!(mesh, eltid, copy(xref), J)
 end
 
 function mesh_print_nodes(mesh)
@@ -182,7 +183,7 @@ function mesh_print_nodes(mesh)
     for i = 1:size(mesh.X,2)
         @printf("%3d : ", i)
         for j = 1:dshapes(mesh.shapes)
-            @printf(" %6.2g", X[i,j])
+            @printf(" %6.2g", mesh.X[j,i])
         end
         @printf("\n")
     end
@@ -190,9 +191,9 @@ end
 
 function mesh_print_elt(mesh)
     @printf("\nElement connectivity:\n")
-    for i = 1:size(mesh.elts,2)
+    for i = 1:size(mesh.elt,2)
         @printf("% 3d :", i)
-        for j = 1:size(mesh.elts,1)
+        for j = 1:size(mesh.elt,1)
             @printf("  % 3d", mesh.elt[j,i])
         end
         @printf("\n")
