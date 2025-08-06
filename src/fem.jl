b75fa1ac1a9b8c7728b0792222db533457637a12
David Bindel <bindel@cornell.edu>
2025-08-06 18:17:32 -0400

    Add partially completed fem.jl

---
+++ src/fem.jl
@@ -0,0 +1,92 @@
+mutable struct FEMProblem{T}
+
+    # Mesh data
+    mesh :: Mesh
+
+    # Element type (NB: can generalize with multiple types)
+    etype :: T
+
+    # Storage for fields
+    U  :: Matrix{Float64}  # Global soln values (ndof-by-numnp)
+    F  :: Matrix{Float64}  # Global force values (ndof-by-numnp)
+    id :: Matrix{Integer}  # Global to reduced ID map (ndof-by-nump)
+
+    # Dimensions
+    ndof :: Integer
+    nactive :: Integer
+
+end
+
+function FEMProblem(mesh, etype, ndof)
+    numnp = size(mesh.X,2)
+    nactive = numnp * ndof
+    U = zeros(ndof, numnp)
+    F = zeros(ndof, numnp)
+    id = zeros(Integer, ndof, numnp)
+    FEMProblem(mesh, etype, U, F, id, ndof, nactive)
+end
+
+function assign_ids(fe :: FEMProblem)
+    nactive = 0
+    for j = 1:size(fe.mesh.X,2)
+        for i = 1:fe.ndof
+            if id[i,j] >= 0
+                nactive += 1
+                id[i] = nactive
+            end
+        end
+    end
+    fe.nactive = nactive
+end
+
+function update_U(fe :: FEMProblem, du_red)
+    for j = 1:size(fe.mesh.X,2)
+        for i = 1:fe.ndof
+            if id[i,j] > 0
+                U[i,j] -= du_red[id[i,j]]
+            end
+        end
+    end
+end
+
+function set_load(fe :: FEMProblem, f :: Function)
+    for i = 1:size(fe.mesh.X,2)
+        f(view(fe.mesh.X,:,i), view(fe.F,:,i))
+    end
+end
+
+function assemble(fe :: FEMProblem, R, K)
+end
+
+
+function fem_print(fe :: FEMProblem)
+    @printf("\nNodal information:\n")
+    @printf("       ID ")
+    for j = 1:dshapes(fe.mesh.shapes)
+        @printf("     X%d", j)
+    end
+    for j = 1:fe.ndof
+        @printf("     U%d", j)
+    end
+    for j = 1:fe.ndof
+        @printf("     F%d", j)
+    end
+    @printf("\n")
+    for i = 1:size(fe.mesh.X,2)
+        @printf("%3d : ", i)
+        for j = 1:ndof
+            @printf("% 3d ", fe.id[i])
+        end
+        for j = 1:dshapes(fe.mesh.shapes)
+            @printf(" %6.2g", fe.mesh.X[j,i])
+        end
+        for j = 1:fe.ndof
+            @printf(" % 6.2g", fe.U[j,i])
+        end
+        for j = 1:fe.ndof
+            @printf(" % 6.2g", fe.F[j,i])
+        end
+        @printf("\n");
+    end
+    mesh_print_elt(fe.mesh)
+end
