e1188633e52ca6580bee037d6d95d0808e3f9e93
David Bindel <bindel@cornell.edu>
2025-08-08 16:55:12 -0400

    Add second FEM test.

--- test/test_fem1d.jl
+++ test/test_fem1d.jl
@@ -33,6 +33,25 @@ function test_fem1(shapes, qrule)
     @test fe.U ≈ fe.mesh.X
 end
 
+function test_fem2(shapes, qrule)
+    fe = setup_test_mesh(6, shapes, qrule, 0.0, 0.0)
+    set_load!(fe, (x,fx) -> fx[1] = 1.0)
+
+    # Set up globals and assemble system
+    R = zeros(fe.nactive)
+    K = zeros(fe.nactive, fe.nactive)
+    assemble!(fe, R, K)
+
+    # Factor, solve, and update
+    update_U!(fe, K\R)
+
+    # Check exactness
+    @test fe.U ≈ fe.mesh.X .* (1.0 .- fe.mesh.X) / 2
+end
+
 test_fem1(Shapes1dP1(), GaussRule1dv(2))
 test_fem1(Shapes1dP2(), GaussRule1dv(3))
 test_fem1(Shapes1dP3(), GaussRule1dv(4))
+
+test_fem2(Shapes1dP2(), GaussRule1dv(3))
+test_fem2(Shapes1dP3(), GaussRule1dv(4))
